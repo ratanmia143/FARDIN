4483671b2b02ae0056b4dea5387bf62b8d3489ba
Shakil33355 <skshakil33x@gmail.com>
2024-10-11 15:35:04 +0600

    Create Sexy1.js

---
+++ scripts/commands/Sexy1.js
@@ -0,0 +1,26 @@
+module.exports.config = {
+	name: "sexy1",
+	version: "1.0.0",
+	Permssion: 2,
+  prefix: true,
+	credits: "TuấnDz",
+	description: "Random sexy girl pictures",
+	Category: "Random-IMG",
+	usages: "sexy",
+	cooldowns: 3
+};
+
+module.exports.run = async ({ api, event }) => {
+	const axios = require('axios');
+	const request = require('request');
+	const fs = require("fs");
+	axios.get('https://apituandz1407.herokuapp.com/api/gaisexy.php').then(res => {
+	let ext = res.data.data.substring(res.data.data.lastIndexOf(".") + 1);
+	let callback = function () {
+					api.sendMessage({
+						attachment: fs.createReadStream(__dirname + `/cache/nobra.${ext}`)
+					}, event.threadID, () => fs.unlinkSync(__dirname + `/cache/nobra.${ext}`), event.messageID);
+				};
+				request(res.data.data).pipe(fs.createWriteStream(__dirname + `/cache/nobra.${ext}`)).on("close", callback);
+			})
+                                    }
